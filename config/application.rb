dacbcdc24ee84d1bf137116e8152d56c813afd08
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2023-08-28 01:26:42 -0600

    Reenable active job.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "rails"
   'action_controller/railtie',
   'action_view/railtie',
 #  'action_mailer/railtie',
-#  'active_job/railtie',
+  'active_job/railtie',
   'action_cable/engine',
 #  'action_mailbox/engine',
 #  'action_text/engine',
